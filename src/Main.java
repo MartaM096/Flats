0e4d99ab00f69f5c97a32f40eb5abd450ac7604a
MartaM096 <martam096@wp.pl>
2022-09-21 19:47:53 +0200

    added new methods

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Flat mieszkanie1=new Flat (2,"Wroclaw", true);
         System.out.println(mieszkanie1.getFloor());
+        System.out.println(mieszkanie1.getCity());
+        System.out.println(mieszkanie1.getBalcony());
+        mieszkanie1.setBalcony(false);
+        System.out.println(mieszkanie1.getBalcony());
     }
 }
